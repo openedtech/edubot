01f0c12c0c1f7bc11ebde67eeef0105360aee31b
exciteabletom <tom@digitalnook.net>
2022-11-30 20:29:08 +0800

    remove unused imports

--- edubot/sql.py
+++ edubot/sql.py
@@ -1,4 +1,3 @@
-import sqlalchemy
 from sqlalchemy import (
     Column,
     DateTime,
@@ -8,7 +7,7 @@ from sqlalchemy import (
     UniqueConstraint,
     create_engine,
 )
-from sqlalchemy.orm import declarative_base, registry, relationship, sessionmaker
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker
 
 from edubot import DATABASE
 
